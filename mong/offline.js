bbc61ce25d0f20e00e97ce1735f3849575bf0936
Valso <warut92@gmail.com>
2021-11-19 23:47:26 +0700

    Add files via upload

---
+++ mong/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1637340033,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite-sheet2.png",
+		"images/sprite2-sheet0.png",
+		"media/mong.m4a",
+		"media/mong.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
