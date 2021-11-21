35da874922ffe66bc5742b905702bb08946fb20b
Valso <warut92@gmail.com>
2021-11-21 18:43:01 +0700

    Add files via upload

---
+++ klong-kheak/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1637494848,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/ting-sheet0.png",
+		"images/ting-sheet1.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"media/klong keak jha_04.m4a",
+		"media/klong keak jha_04.ogg",
+		"media/klong keak jho_04.m4a",
+		"media/klong keak jho_04.ogg",
+		"media/klong keak ting_04.m4a",
+		"media/klong keak ting_04.ogg",
+		"media/klong keak tum_04.m4a",
+		"media/klong keak tum_04.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
