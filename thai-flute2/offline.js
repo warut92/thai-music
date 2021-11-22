575d0b04bbb8793384893c192df49bb01813ecdb
Valso <warut92@gmail.com>
2021-11-22 15:35:15 +0700

    Add files via upload

---
+++ thai-flute2/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1637569930,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite3-sheet1.png",
+		"media/a3 long.m4a",
+		"media/a3 long.ogg",
+		"media/b3 long.m4a",
+		"media/b3 long.ogg",
+		"media/g3 long.m4a",
+		"media/g3 long.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
